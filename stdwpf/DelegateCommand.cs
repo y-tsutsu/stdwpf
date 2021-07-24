fc128c28769f6553b46d6e0b80b484a4563ca360
tsutsu <tsutsu0928@gmail.com>
2021-07-25 02:52:43 +0900

    標準機能でのデータバインドを実装

---
+++ stdwpf/DelegateCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace stdwpf
+{
+    class DelegateCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private Func<bool> canExecute = null;
+
+        private Action execute = null;
+
+        public DelegateCommand(Action execute) : this(execute, () => true) { }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => this.canExecute();
+
+        public void Execute(object parameter) => execute();
+    }
+}
---
+++ stdwpf/MainWindowViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace stdwpf
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private string textValue = string.Empty;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ButtonCommand { get; }
+
+        public string TextValue
+        {
+            get
+            {
+                return this.textValue;
+            }
+            set
+            {
+                if (value != this.textValue)
+                {
+                    this.textValue = value;
+                    this.RaisePropertyChanged();
+                }
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.TextValue = "Hello, MVVM!!";
+            this.ButtonCommand = new DelegateCommand(
+                () => this.TextValue = "Clicked!!",
+                () => !string.IsNullOrEmpty(this.TextValue));
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+            => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
